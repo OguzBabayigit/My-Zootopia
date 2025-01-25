20e79e5f580d424cdd7148c2ec9a9b79c7ff928d
Oguz Babayigit <babayigitoguz90@gmail.com>
2025-01-25 14:14:34 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,30 @@
+import json
 
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+
+animals_data = load_data('animals_data.json')
+
+def serialize_animal(animal):
+    animal_name = animal['name']
+    animal_diet = animal['taxonomy']['order']
+    animal_location = ', '.join(animal['locations'])
+    animal_type = animal['characteristics'].get('type', 'Unknown')
+
+    # Create the HTML list item with the animal data
+    return f'''
+<li class="cards__item">
+    <div class="card__title">{animal_name}</div><br/>
+    <p class="card__text">
+        <strong>Diet:</strong> {animal_diet}<br/>
+        <strong>Location:</strong> {animal_location}<br/>
+        <strong>Type:</strong> {animal_type}<br/>
+    </p>
+</li>
+'''
+
+for animal in animals_data:
+    print(serialize_animal(animal))
